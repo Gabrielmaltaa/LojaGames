31bc84e2f33dd8b13646c334bc41b214dce55f95
Gabriel Malta <gabrielmaltaa2@gmail.com>
2026-03-03 22:31:43 -0300

    Codigo do projeto finalizado, realmente agora atende os requisitos do projeto, porem falta alguns pequenos ajuste

---
+++ src/Constantes.java
@@ -0,0 +1,3 @@
+public class Constantes {
+    public static final String TITULO_JANELA = "LOJA DE GAMES RETRÔ";
+}
--- src/JanelaRetro.java
+++ src/JanelaRetro.java
@@ -9,35 +9,32 @@ public class JanelaRetro {
 
     public JanelaRetro() {
         pagamento = new Pagamento();
-        janela = new JFrame("APP");
+        janela = new JFrame("LOJA DE GAMES RETRÔ");
         janela.setSize(620,350);
         janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         janela.setLayout(null);
 
-        jogoSuperAventura = new JButton("Adicionar Super Aventura: ");
-        jogoSuperAventura.setBounds(20,20,190,60);
+        jogoSuperAventura = new JButton("Comprar Super Aventura");
+        jogoSuperAventura.setBounds(20,20,200,70);
 
-        jogoCombateEspacial = new JButton("Adicionar Combate Espacial: ");
-        jogoCombateEspacial.setBounds(20,100,190,60);
+        jogoCombateEspacial = new JButton("Comprar Combate Espacial");
+        jogoCombateEspacial.setBounds(20,110,200,70);
 
-        jogoCorridaTurbo = new JButton("Adicionar Corrida Turbo");
-        jogoCorridaTurbo.setBounds(20,180,190,60);
+        jogoCorridaTurbo = new JButton("Comprar Corrida Turbo");
+        jogoCorridaTurbo.setBounds(20,200,200,70);
 
 
         JLabel labelTotal = new JLabel("Total: ");
-        labelTotal.setBounds(190,40,120,30);
-
+        labelTotal.setBounds(230,40,160,70);
 
         valorTotal = new JLabel("0");
-        valorTotal.setBounds(340,40,120,30);
-
+        valorTotal.setBounds(290,40,160,70);
 
         JLabel labelTotalComDesconto = new JLabel("Total Com Desconto: ");
-        labelTotalComDesconto.setBounds(190,100,120,30);
-
+        labelTotalComDesconto.setBounds(230,90,160,70);
 
         valorTotalComDesconto = new JLabel("0");
-        valorTotalComDesconto.setBounds(340,100,120,30);
+        valorTotalComDesconto.setBounds(370,90,160,70);
 
         janela.add(jogoCombateEspacial);
         janela.add(jogoSuperAventura);
@@ -49,13 +46,36 @@ public class JanelaRetro {
 
         criarEventos();
     }
+    public void criarEventos(){
 
-    public void criarEventos() {
+        Jogos superAventura = new Jogos("Super Aventura 64", 45.00);
+        Jogos combateEspacial = new Jogos("Combate Espacial", 60.00);
+        Jogos corridaTurbo = new Jogos("Corrida Turbo", 50.00);
 
 
+        jogoCombateEspacial.addActionListener(e->{
+        pagamento.adicionarJogos(combateEspacial, 1);
+        atualizarTotais();
+    });
+        jogoCorridaTurbo.addActionListener(e->{
+            pagamento.adicionarJogos(corridaTurbo, 1);
+            atualizarTotais();
+        });
+        jogoSuperAventura.addActionListener(e->{
+            pagamento.adicionarJogos(superAventura, 1);
+            atualizarTotais();
+        });
 
-    }
 
+}
+
+public void atualizarTotais() {
+    double total = pagamento.getTotal();
+    double totalComDesconto = pagamento.getTotalComDesconto();
+
+    valorTotal.setText("R$ " + total );
+    valorTotalComDesconto.setText("R$ " + totalComDesconto);
+}
     public void mostrarJanela() {
         // Defini para ficar visivel
         janela.setVisible(true);
